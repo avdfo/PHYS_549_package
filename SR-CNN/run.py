65e1ce4ceb14daebad847b7ecb2ccc039a3ceb44
avdfo <112825225+avdfo@users.noreply.github.com>
2022-12-09 14:58:20 -0600

    Add files via upload

--- SR-CNN/run.py
+++ SR-CNN/run.py
@@ -19,14 +19,16 @@ def main():
     sess.run(tf.compat.v1.global_variables_initializer())
     saver = tf.compat.v1.train.Saver()
 
-    tfp.training(sess=sess, neuralnet=srnet, saver=saver, dataset=dataset, epochs=FLAGS.epoch, batch_size=FLAGS.batch)
-    tfp.validation(sess=sess, neuralnet=srnet, saver=saver, dataset=dataset)
+    tfp.training(sess=sess, neuralnet=srnet, saver=saver, dataset=dataset, epochs=FLAGS.epoch, batch_size=FLAGS.batch, batch_size_val=FLAGS.batch_val)
+    tfp.testing(sess=sess, neuralnet=srnet, saver=saver, dataset=dataset)
+    tfp.testing_exp(sess=sess, neuralnet=srnet, saver=saver, dataset=dataset)
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epoch', type=int, default=20, help='-')
+    parser.add_argument('--epoch', type=int, default=200, help='-')
     parser.add_argument('--batch', type=int, default=16, help='-')
+    parser.add_argument('--batch_val', type=int, default=4, help='-')
 
     FLAGS, unparsed = parser.parse_known_args()
 
